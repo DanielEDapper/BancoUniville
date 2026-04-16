3de90677161f810f9bc60975ec51e6bd54077f4a
Gustavo Joaquim <gustavo_vale@estudante.sesisenai.org.br>
2026-04-16 10:55:00 -0300

    Update index.py

--- univille_banco/index.py
+++ univille_banco/index.py
@@ -1,26 +1,40 @@
-user = str(input("Digite o nome do seu usuário que desejar cadastrar: "))
-senha = float(input("Crie uma senha para o acesso:"))
+def main():
+    user = input("Cadastre o usuário: ")
+    senha = input("Crie uma senha: ")
 
-escolha = 0
-usuario_digitado = 0
-senha_digitada = 0
-i = 0
 
-print("- UNIVILLE Internet Banking -")
-print("\n")
+    while True:
+        print("\n--- UNIVILLE Internet Banking ---")
+        print("1 - Acessar")
+        print("2 - Encerrar")
 
-escolha= int(input("Digite 1 para Acessar o programa ou digite 2 para Encerrar o programa"))
+        escolha = int(input("\nEscolha uma opção: "))
 
-if escolha == 1:
-    usuario_digitado = str(input("Digite o seu usuário:"))
-    senha_digitada = float(input("Digite a sua senha:"))
+        if escolha == 1:
+            tentativas = 0
 
-    if usuario_digitado != user and senha_digitada !=:
-        while i > 3:
-            usuario_digitado = str(input("Digite o seu usuário:"))
-            if usuario_digitado == user:
+            while tentativas < 3:
+                tentativas_disponiveis = 2 - tentativas
+                usuario_digitado = input("\nUsuário: ")
+                senha_digitada = input("Senha: ")
 
-def menuInicial():
-    {
-        
-    }
+                if usuario_digitado == user and senha_digitada == senha:
+                    print("\nAcesso permitido!")
+                    break
+                else:
+                    print("\nUsuário ou senha incorretos!")
+                    tentativas += 1
+                    print(f"Número de tentativas: {tentativas_disponiveis}")
+
+            if tentativas == 3:
+                print("Acesso bloqueado!")
+
+        elif escolha == 2:
+            print("Programa encerrado!")
+            break
+
+        else:
+            print("Opção inválida!")
+
+
+main()
