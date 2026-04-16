e52e0d4d2415761c01762aaa3f827a2f51b75725
Daniel Erthal Dapper <danielerthaldapper2008@gmail.com>
2026-04-16 10:59:11 -0300

    Add banking menu and user balance functionality

--- univille_banco/index.py
+++ univille_banco/index.py
@@ -1,6 +1,7 @@
 def main():
     user = input("Cadastre o usuário: ")
     senha = input("Crie uma senha: ")
+    saldo = 131
 
 
     while True:
@@ -20,6 +21,7 @@ def main():
 
                 if usuario_digitado == user and senha_digitada == senha:
                     print("\nAcesso permitido!")
+                    menuInicial(saldo)
                     break
                 else:
                     print("\nUsuário ou senha incorretos!")
@@ -35,6 +37,55 @@ def main():
 
         else:
             print("Opção inválida!")
+
+def menuInicial(saldo):
+    continuar = True
+
+    while continuar:
+        print("===================================")
+        print("==   UNIVILLE Internet Banking   ==")
+        print("===================================")
+        print("==   1 - Consultar Saldo         ==")
+        print("==   2 - Realizar Saque          ==")
+        print("==   3 - Realizar Depósito       ==")
+        print("==   4 - Consultar limite        ==")
+        print("==   5 - Encerrar                ==")
+        print("===================================")
+
+        opcao = int(input("Digite a opção desejada: "))
+
+        match opcao:
+            case 1:
+                menuSaldo(saldo)
+            case 2:
+                print("Realizar Saque Solicitado")
+            case 3: 
+                print("Realizar Depósito Solicitado")
+            case 4:
+                print("Consultar Limite Solicitado")
+            case 5:
+                print("Encerrar Sistema Solicitado")
+                continuar = False
+
+def menuSaldo(saldo):
+    print("===================================")
+    print("==             SALDO             ==")
+    print("===================================")
+
+    print("                                   ")
+    print(f"Saldo: R$ {saldo:.2f}")
+
+def menuSaque():
+    print("Realizar Saque Solicitado")
+
+def menuDeposito():
+    print("Realizar Depósito Solicitado")
+
+def menuLimite():
+    print("Consultar Limite Solicitado")
+
+def menuRetornar():
+    print("Encerrar Sistema Solicitado")
 
 
 main()
