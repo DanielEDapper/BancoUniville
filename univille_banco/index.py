164b97df0c08fd4f0b0f7467aa6c95924793c63a
Gustavo Joaquim <gustavo_vale@estudante.sesisenai.org.br>
2026-04-16 10:00:54 -0300

    Add files via upload

---
+++ univille_banco/index.py
@@ -0,0 +1,21 @@
+user = str(input("Digite o nome do seu usuário que desejar cadastrar: "))
+senha = float(input("Crie uma senha para o acesso:"))
+
+escolha = 0
+usuario_digitado = 0
+senha_digitada = 0
+i = 0
+
+print("- UNIVILLE Internet Banking -")
+print("\n")
+
+escolha= int(input("Digite 1 para Acessar o programa ou digite 2 para Encerrar o programa"))
+
+if escolha == 1:
+    usuario_digitado = str(input("Digite o seu usuário:"))
+    senha_digitada = float(input("Digite a sua senha:"))
+
+    if usuario_digitado != user and senha_digitada !=:
+        while i > 3:
+            usuario_digitado = str(input("Digite o seu usuário:"))
+            if usuario_digitado == user:
